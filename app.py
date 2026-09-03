6b75cf282230172a8e7776bb118697e3f50ae6e0
eliseus10top-ctrl <eliseus10top@gmail.com>
2026-09-02 21:31:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2950,3 +2950,6 @@ print(
 # ================================================================
 
 pesquisar_ticker()
+if __name__ == "__main__":
+    demo.launch(server_name="0.0.0.0", server_port=10000)
+
